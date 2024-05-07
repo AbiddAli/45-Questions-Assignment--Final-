bb9e5a5ccd5211072272212f08dbae84c1409b89
Abid Ali <160251628+AbiddAli@users.noreply.github.com>
2024-05-08 01:59:03 +0500

    first commit

---
+++ Question no. 10/main.ts
@@ -0,0 +1,15 @@
+// Q-10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+// My Name is Abid
+// Dated 01-05-2024
+console.log("Thanks to the team of IT Initiative Program");
+
+
+// My Name is Abid
+// Dated 01-05-2024
+// Student of Generative IT Program.
+console.log("Hello World Simple Program");
+
+
+
