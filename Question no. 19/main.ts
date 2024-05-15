5f04af4d9f09abc4b7dcd49ce5c00dab407c942d
Abid Ali <160251628+AbiddAli@users.noreply.github.com>
2024-05-16 02:29:05 +0500

    first commit

---
+++ Question no. 19/main.ts
@@ -0,0 +1,8 @@
+// Q-19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guest_invite: string [] = ["Ibrahim", "Ahmed"]
+
+let count_invite: number = guest_invite.length
+
+console.log(`${count_invite} Guests are invited for the dinner!`);
+
