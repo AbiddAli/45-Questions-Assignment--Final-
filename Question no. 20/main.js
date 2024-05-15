bbeb60a2bcff2ae6d3c53c7df08b5e0c784298c1
Abid Ali <160251628+AbiddAli@users.noreply.github.com>
2024-05-16 02:34:34 +0500

    first commit

---
+++ Question no. 20/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let languages = ["Arabic", "English", "Urdu", "Chinese", "German"];
+console.log("List of Languages:");
+for (let list of languages) {
+    console.log(list);
+}
+;
---
+++ Question no. 20/main.ts
@@ -0,0 +1,13 @@
+// Q-20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
+let languages: string [] = ["Arabic","English","Urdu","Chinese","German"];
+
+console.log("List of Languages:");
+
+for (let list of languages){console.log(list)};
+
+
+
+
+
