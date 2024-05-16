f323b92152cbd86f4357531a2498f8e2cbaa9547
Abid Ali <160251628+AbiddAli@users.noreply.github.com>
2024-05-16 14:48:17 +0500

    first commit

---
+++ Question no. 23/main.js
@@ -0,0 +1,33 @@
+"use strict";
+// Conditional Tests
+let car = "subaru";
+// Test # 1
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test # 2
+console.log("is car != 'Honda Civic'? I predict True");
+console.log(car != 'Honda Civic');
+// Test # 3
+console.log("is car == 'subaru'? I predict False");
+console.log(car == 'subaru');
+// Test # 4
+console.log("is car == 'SUBARU'? I predict False");
+console.log(car == 'SUBARU');
+// Test # 5
+console.log("is car.length == 6? I predict True");
+console.log(car.length == 6);
+// Test # 6
+console.log("is car.length != 10? I predict True");
+console.log(car.length != 10);
+// Test # 7
+console.log("is 10 > 50? I predict false");
+console.log(10 > 50);
+// Test # 8
+console.log("is 9 <= 6? I predict false");
+console.log(9 <= 6);
+// Test # 9
+console.log("is 83 > 89? I predict false");
+console.log(83 > 89);
+// Test # 10
+console.log("is car == 'subaru' && car.length = 8? I predict true");
+console.log(car == 'subaru' && car.length == 8);
---
+++ Question no. 23/main.ts
@@ -0,0 +1,51 @@
+// Q-23.Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+// Conditional Tests
+let car = "subaru";
+
+// Test # 1
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+// Test # 2
+console.log("is car != 'Honda Civic'? I predict True");
+console.log(car != 'Honda Civic');
+
+// Test # 3
+console.log("is car == 'subaru'? I predict False");
+console.log(car == 'subaru');
+
+// Test # 4
+console.log("is car == 'SUBARU'? I predict False");
+console.log(car == 'SUBARU');
+
+// Test # 5
+console.log("is car.length == 6? I predict True")
+console.log(car.length == 6);
+
+// Test # 6
+console.log("is car.length != 10? I predict True")
+console.log(car.length != 10);
+
+// Test # 7
+console.log("is 10 > 50? I predict false");
+console.log(10 > 50)
+
+// Test # 8
+console.log("is 9 <= 6? I predict false");
+console.log(9 <= 6)
+
+// Test # 9
+console.log("is 83 > 89? I predict false");
+console.log(83 > 89)
+
+// Test # 10
+console.log("is car == 'subaru' && car.length = 8? I predict true");
+console.log(car == 'subaru' && car.length == 8)
+
